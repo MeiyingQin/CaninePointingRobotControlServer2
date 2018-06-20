4f796b2ae48693f951df8a6cb4a5ba4417aec6cf
Meiying Qin <meiyingqin@gmail.com>
2018-06-20 14:41:48 -0400

    update the audio split script

--- util/segment_audio.py
+++ util/segment_audio.py
@@ -1,24 +1,62 @@
+import pydub
 from pydub import AudioSegment
 
-audio_file = AudioSegment.from_mp3("sentences.mp3")
+raw_audio_file = AudioSegment.from_mp3("./sentences.MP3")
 
-num_average_milisecond = 5
+chunks = pydub.silence.split_on_silence(raw_audio_file, min_silence_len=500, silence_thresh=-30, keep_silence=500)
 
-current_average = []
+file_name = 0
+for audio in chunks:
+    audio.export(str(file_name) + ".mp3", format="mp3")
+    file_name += 1
 
-for count in range(num_average_milisecond):
-    current_average.append(audio_file[count])
-    
+"""
+num_average_milisecond = 500
+
+start_point = 0
 start = 0
 end = 0
 file_name = 1
 is_recording = False
+threshold = -16
 
-for milisecond in range(5, len(audio_file)):
-    current_average.pop(0)
-    current_average.append(audio_file[milisecond])
+
+print "loop through video"
+milisecond = 0
+for milisecond in range(len(audio_file)):
+    current_average = []
+    sound_level = audio_file[milisecond].dBFS
+    
+    if sound_level != -float("infinity"):
+        if len(current_average) >= num_average_milisecond:
+            current_average.pop(0)
+        current_average.append(sound_level)
+    else:
+        print "loop next one"
+        continue
+    
+    current_value = sum(current_average) / len(current_average)
+    #print "current value", current_value
     
     if is_recording:
-        pass
+        if current_value < threshold:
+            print "found end"
+            #end = milisecond - num_average_milisecond + 500
+            end = milisecond - num_average_milisecond
+            print "(start, end)", start, end
+            #audio_file[start:end].export(str(file_name) + ".mp3", format="mp3")
+            file_name += 1
+            is_recording = False
+            current_average = []
+            start = end
     else:
-        pass
+        if current_value > threshold:
+            print "found start"
+            #start = milisecond - num_average_milisecond - 500
+            start = milisecond - num_average_milisecond
+            if start < 0:
+                start = milisecond - num_average_milisecond
+            is_recording = True
+
+print "total fild generaged", file_name
+"""
