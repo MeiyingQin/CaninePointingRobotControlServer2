6be031e17f3ef27d3303bc831423661901f3b7dc
Meiying Qin <meiyingqin@gmail.com>
2018-07-04 19:35:47 -0400

    finish idling, blinking, receiving signals other than initial participant information

---
+++ bait.py
@@ -0,0 +1,46 @@
+import selenium
+from selenium import webdriver
+import time
+import threading
+
+class Dispenser():
+    def __init__(self, user_email, user_password):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://portal.feedandgo.com/")
+        email = self.driver.find_element_by_id("user_email")
+        password = self.driver.find_element_by_id("user_password")
+        submit_button = self.driver.find_element_by_class_name("login_submit_btn")
+        email.send_keys(user_email)
+        password.send_keys(user_password)
+        submit_button.click()
+    
+    def feed(self):
+        is_found = False
+        while not is_found:
+            try:
+                feedNowButton = self.driver.find_element_by_partial_link_text("Feed Now")
+                feedNowButton.click()
+                is_found = True
+            except selenium.common.exceptions.ElementNotVisibleException:
+                print "wait for logging in..."
+        
+        is_found = False
+        while not is_found:
+            try:
+                feedConfirmButton = self.driver.find_element_by_id("feed-yes")
+                feedConfirmButton.click()
+                is_found = True
+            except selenium.common.exceptions.ElementNotVisibleException:
+                print "wait for loading"
+        
+        is_found = False
+        while not is_found:
+            try:
+                buttonParent = self.driver.find_element_by_id("callPetOver")
+                cancelWebcamButton = buttonParent.find_element_by_css_selector(".btn.btn-default.left")
+                cancelWebcamButton.click()
+                is_found = True
+            except selenium.common.exceptions.ElementNotVisibleException:
+                print "wait for finishing"
+        
+        time.sleep(10)
