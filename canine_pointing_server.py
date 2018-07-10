e16a9f3bdd815de88bede610effd1acab19856d0
Meiying Qin <meiyingqin@gmail.com>
2018-07-10 11:34:22 -0400

    update json

--- canine_pointing_server.py
+++ canine_pointing_server.py
@@ -93,6 +93,7 @@ class naoRobot(object):
         #self.ttsProxy.resetSpeed()
     
     def speech(self, text):
+        print "in speech text", text
         self.animatedSpeechProxy.say(text)
     
     def stand(self, speed):
@@ -101,6 +102,7 @@ class naoRobot(object):
     def initialize(self):
         #self.ttsProxy.setParameter("volume", 82)
         self._disable_notifications()
+        self.animatedSpeechProxy.setBodyLanguageModeFromStr("disabled")
     
     #def torso_stand(self, speed=None):
         #left_arm = [83.4, 10.4, -24, -67.5, 6.7, 0.3]
@@ -170,7 +172,7 @@ class naoRobot(object):
         while self.is_running_idling:
             time.sleep(0.1)
         
-        self.stand()
+        self.stand(0.2)
     
     def start_blinking(self):
         self.blinking = True
@@ -184,7 +186,7 @@ class naoRobot(object):
             
             intensity = 1.0
             duration = 0.2
-            proxy.fade(name, intensity, duration)
+            self.ledProxy.fade(name, intensity, duration)
             
             custom_print("blink")
 
@@ -212,9 +214,11 @@ def custom_print(message):
     sys.stdout.flush()
 
 def dispenser_rotate(dispenser_lists):
+    pass
+    """
     threads = []
     for dispenser in dispenser_lists:
-        dispenser_thread = theading.Thread(target=Dispenser.feed, args=(dispenser, ))
+        dispenser_thread = threading.Thread(target=Dispenser.feed, args=(dispenser, ))
         threads.append(dispenser_thread)
     
     for thread in threads:
@@ -222,6 +226,7 @@ def dispenser_rotate(dispenser_lists):
     
     for thread in threads:
         thread.join()
+    """
 
 def is_initial_information(command):
     command_list = command.strip().split(DELIMINATOR)
@@ -246,8 +251,10 @@ def parse_command(command, library, dog, dog_gender, owner, pointer, assistant):
     return command_content, flag
 
 def run_command(nao, commands, flags, dispensers):
+    print commands
     for flag_index in range(len(flags)):
         command = commands[flag_index]
+        command = str(command)
         if flags[flag_index] == DICT_FLAG_SPEECH:
             command = command.replace(DICT_OWNER_TAG, owner)
             command = command.replace(DICT_DOG_TAG, dog)
@@ -255,10 +262,9 @@ def run_command(nao, commands, flags, dispensers):
             command = command.replace(DICT_POINTER_TAG, pointer)
             command = command.replace(DICT_ASSISTANT_TAG, assistant)
             print "run speech command: ", command
-            #nao.speech(command)
+            nao.speech(command)
         elif flags[flag_index] == DICT_FLAG_ACTION:
             print "run action command: ", command
-            """
             if command == "start_idle":
                 idle_thread = threading.Thread(target=naoRobot.start_idle, args=(nao, ))
                 idle_thread.start()
@@ -266,12 +272,11 @@ def run_command(nao, commands, flags, dispensers):
             elif command == "stop_idle":
                 nao.stop_idle()
             elif command == "stand":
-                nao.stand(0.2)
+                nao.stand(0.3)
             elif command == "dispenser_rotate":
                 dispenser_rotate(dispensers)
             else:
                 custom_print("command is not defined")
-            """
         else:
             custom_print(flag)
             custom_print("this is not a valid flag")
@@ -307,8 +312,8 @@ if __name__ == "__main__":
     
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #server_ip = "192.168.1.101"
-    server_ip = "192.168.7.142"
-    server_port = 10009
+    server_ip = "192.168.1.116"
+    server_port = 10000
     server_address = (server_ip, server_port)
     socket.bind(server_address)
     socket.listen(1)
@@ -316,8 +321,7 @@ if __name__ == "__main__":
     file_name = "log_robot_actions.txt"
     log(file_name, "=======================================================================\n")
     
-    #nao = naoRobot(robot_ip, robot_port)
-    nao = ""
+    nao = naoRobot(robot_ip, robot_port)
     
     custom_print("server ip: " + server_ip)
     custom_print("server port: " + str(server_port))
@@ -334,8 +338,8 @@ if __name__ == "__main__":
             custom_print("get connection from " + str(client_address))
             log(file_name, "get connection from " + str(client_address))
             
-            #blinking_thread = threading.Thread(target=naoRobot.start_blinking, args=(nao, ))
-            #blinking_thread.start()
+            blinking_thread = threading.Thread(target=naoRobot.start_blinking, args=(nao, ))
+            blinking_thread.start()
             
             dog = ""
             dog_gender = ""
@@ -362,10 +366,12 @@ if __name__ == "__main__":
                     custom_print("received empty data, close socket")
                     log(file_name, "close socket")
                     is_connected = False
-                    #nao.stop()
-                    #blinking_thread.join()
+                    nao.stop()
+                    blinking_thread.join()
             
             connection.close()
     except KeyboardInterrupt:
         socket.close()
         custom_print("main server receive keyboard interruption, close socket and shut server down")
+        nao.stop()
+        blinking_thread.join()        
