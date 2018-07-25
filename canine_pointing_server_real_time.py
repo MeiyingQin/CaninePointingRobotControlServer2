e2894582e956a061539382ec95b3baac5aab6e11
MeiyingQin <meiyingqin@gmail.com>
2018-07-25 16:39:58 -0400

    add robot walk to real time server

--- canine_pointing_server_real_time.py
+++ canine_pointing_server_real_time.py
@@ -11,6 +11,12 @@ def progressToAngle(progress):
 def angleToProgress(angle):
     return int(round((angle / almath.TO_RAD) * PROGRESS_BAR_RANGE / ROBOT_HEAD_RANGE, 1))
 
+def walk(motion, x, y, z, speed=0.5):
+    # x: forward is x, backward is y
+    # y: side way, left is +, right is -
+    # z: turn, left is +, right is -
+    motion.moveToward(speed * x, speed * y, speed * z)
+
 def custom_print(message):
     print message
     sys.stdout.flush()
@@ -23,16 +29,27 @@ if __name__ == "__main__":
     DELIMINATOR = "|"
     GET_HEAD_ANGLE = "get head angle"
     SET_HEAD_ANGLE = "set head angle"
+    WALK_INIT = "walk init"
+    WALK_FORWARD = "walk forward"
+    WALK_BACKWARD = "walk backward"
+    WALK_LEFT = "walk left"
+    WALK_RIGHT = "walk right"    
+    WALK_TURN_LEFT = "turn left"
+    WALK_TURN_RIGHT = "turn right"
+    WALK_STOP = "walk stop"
+    STAND = "stand"
+    
     PROGRESS_BAR_RANGE = 100.0
     ROBOT_HEAD_RANGE = 75.0
     
     robot_ip = "192.168.1.102"
     robot_port = 9559
     robot_speed = 0.1
-    motionProxy = ALProxy("ALMotion", robot_ip, robot_port)   
+    motionProxy = ALProxy("ALMotion", robot_ip, robot_port)
+    postureProxy = ALProxy("ALRobotPosture", robot_ip, robot_port)
     
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = "192.168.1.116"
+    server_ip = "192.168.1.112"
     server_port = 10001
     server_address = (server_ip, server_port)
     socket.bind(server_address)
@@ -60,6 +77,26 @@ if __name__ == "__main__":
                     elif data[0] == SET_HEAD_ANGLE:
                         angle = progressToAngle(data[1])
                         motionProxy.setAngles("HeadYaw", [angle], robot_speed)
+                    elif data[0] == WALK_INIT:
+                        motionProxy.moveInit()
+                    elif data[0] == WALK_FORWARD:
+                        walk(motionProxy, 1, 0, 0)
+                    elif data[0] == WALK_BACKWARD:
+                        walk(motionProxy, -1, 0, 0)
+                    elif data[0] == WALK_TURN_LEFT :
+                        walk(motionProxy, 0, 0, 1)
+                    elif data[0] == WALK_TURN_RIGHT:
+                        walk(motionProxy, 0, 0, -1)
+                    elif data[0] == WALK_LEFT :
+                        walk(motionProxy, 0, 1, 0)
+                    elif data[0] == WALK_RIGHT:
+                        walk(motionProxy, 0, -1, 0)                   
+                    elif data[0] == WALK_STOP:
+                        motionProxy.stopMove()
+                    elif data[0] == STAND:
+                        postureProxy.goToPosture("Stand", 0.3)
+                    else:
+                        print "invalid command"
                     custom_print("sent: " + message)
                     connection.sendall(message)
                 else:
